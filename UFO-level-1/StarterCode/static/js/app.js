6856791203ca27498f7579bb988bd83c8159b654
Christine Mazur <M246172@na.merckgroup.com>
2019-11-16 08:19:16 -0500

    more

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,6 @@
 // from data.js
+//vaiables
+//tableData gets data from data.js file
 var tableData = data;
 var filterType = d3.select("#filter-type");
 var filterTypeValue = d3.select("#filter-type-value");
@@ -8,17 +10,15 @@ var tbody = d3.select("tbody");
 console.log(tableData);
 autoPopulate(tableData);
 
+//fill the data into the table
 function autoPopulate(tableData) {
 
-    // Use d3 to automatically populate tableData by creating table rows
-// and cells.
+// create table rows (tr) and data elements (td)      
 
-tableData.forEach((alients) => {
-    // Create rows
+tableData.forEach((aliens) => {
     var row = tbody.append("tr");
    
-    Object.entries(alients).forEach(([key, value]) => {
-        // Create cells
+    Object.entries(aliens).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
     });
