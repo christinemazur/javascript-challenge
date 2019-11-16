f2feaad48dd481de5133b437150fdb6b2651652e
Christine Mazur <M246172@na.merckgroup.com>
2019-11-16 08:39:48 -0500

    more

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -25,4 +25,50 @@ tableData.forEach((aliens) => {
 });
 
 }
-// YOUR CODE HERE!
+//Use a date form in your HTML document and write JavaScript code that will 
+//listen for events and search through the date/time column to find rows that 
+//match user input.
+// when clicking the filter button
+submit.on("click", function() {
+        
+    // no refreshing
+    d3.event.preventDefault();
+
+    // clear the  table
+    tbody.html("");
+
+    //get the data from box
+    var inputElement = d3.select("#filtertype");
+     
+    var inputValue = inputElement.property("value");
+    
+    if (inputValue == '') {
+        alert("Enter a Date");
+        document.getElementById("#filtertype").focus();
+        autoPopulate(tableData);
+    }
+    
+    //Filter by date
+    var typeVal = d3.select("label").attr("for");
+    
+    var filteredData = tableData.filter(alients => alients[typeVal] === inputValue.toLowerCase());
+    if (filteredData.length == 0) {
+        alert("None Found");
+        d3.select("#filtertype").node().value = '';
+        autoPopulate(tableData);
+    }
+    console.log(filteredData);
+    
+    //Displaying the data by date
+    filteredData.forEach((aliens) => {
+        // Create a row
+        var row = tbody.append("tr");
+        //go thru each row
+        Object.entries(aliens).forEach(([key, value]) => {
+            // Creating cells for posting table data
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});  //submit
+
