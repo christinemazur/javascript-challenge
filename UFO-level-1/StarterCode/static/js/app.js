004d775a80f7ee1f7daebe27a0e4cd389fbefb0e
Christine Mazur <M246172@na.merckgroup.com>
2019-11-16 08:07:14 -0500

    more

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
+var filterType = d3.select("#filter-type");
+var filterTypeValue = d3.select("#filter-type-value");
+var submit = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
 
+console.log(tableData);
+autoPopulate(tableData);
+
+function autoPopulate(tableData) {
+
+    // Use d3 to automatically populate tableData by creating table rows
+// and cells.
+
+tableData.forEach((alients) => {
+    // Create rows
+    var row = tbody.append("tr");
+   
+    Object.entries(alients).forEach(([key, value]) => {
+        // Create cells
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+}
 // YOUR CODE HERE!
